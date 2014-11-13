ddbf7a7b544e48cb3de4a3cd6a666ebc111d702d
dmelass <dmelass@gmail.com>
2014-11-13 09:03:35 -0500

    final version of this, moving in new direction

--- data_gathering.py
+++ data_gathering.py
@@ -1,9 +1,7 @@
 # Dara Elass
 
 from auth_ids import echonest_API_key, spotify_API_key
-from pyechonest import song
-from pyechonest import artist
-from pyechonest import config
+from pyechonest import song, artist, config
 import musixmatch
 import urllib2
 from bs4 import BeautifulSoup
@@ -12,133 +10,149 @@ import re
 import time
 from datetime import datetime
 import json
+from string import digits, translate
 
 ######################################################################################
 # # general functions
 ######################################################################################
 
-def create_pickle(data):
-    file_name = str(data)+'.pkl'
-    with open(file_name,'w') as picklefile:
+
+def create_pickle(data, filename):
+    with open(filename,'w') as picklefile:
         pickle.dump(data,picklefile)
 
 def load_pickle(filename):
     data_name = filename[:-4]
-    print data_name
     with open(filename,'r') as picklefile:
         data_name = pickle.load(picklefile)
     return data_name
+
+# remove anything after '(', inclusive
+def remove_paran(name):
+    paran_loc = name.index('(')
+    name = name[:paran_loc].strip()
+    return name
+
+# remove anything after '-', inclusive
+def remove_dashes(name):
+    paran_loc = name.index('-')
+    name = name[:paran_loc].strip()
+    return name
+
+# some song names have numbers to represent arabic letters not in english
+def remove_numbers(name):
+    return re.sub('\d','',name)
 
 ######################################################################################
 # # webscraping lyricstranslate.com for all Arabic -> English lyrics
 ######################################################################################
 
-# # connect using beautiful soup
-# def connect(url):
-#     page = urllib2.urlopen(url)
-#     return BeautifulSoup(page.read(), 'html5lib')
+# connect using beautiful soup
+def connect(url):
+    page = urllib2.urlopen(url)
+    return BeautifulSoup(page.read(), 'html5lib')
 
-# # get urls of all pages
-# def get_page_urls(num_pages):
-#     translations_url_first = "http://lyricstranslate.com/en/translations/12/328/none/none/none"
-#     translations_url_others = "http://lyricstranslate.com/en/translations/12/328/none/none/none-page-"
-#     all_page_urls = [translations_url_first]
-#     for i in range(num_pages-1): 
-#         all_page_urls.append(translations_url_others+str(i+1))
-#     return all_page_urls
+# get urls of all pages
+def get_page_urls(num_pages):
+    translations_url_first = "http://lyricstranslate.com/en/translations/12/328/none/none/none"
+    translations_url_others = "http://lyricstranslate.com/en/translations/12/328/none/none/none-page-"
+    all_page_urls = [translations_url_first]
+    for i in range(num_pages-1): 
+        all_page_urls.append(translations_url_others+str(i+1))
+    return all_page_urls
 
-# # get urls of each translation
-# def get_all_lyrics_urls(page_urls):
-#     lyrics_urls = []
-#     for page_url in page_urls:
-#         page_soup = connect(page_url)
-#         for row in page_soup.findAll('tr')[1:]:
-#             column = row.findAll('td',{'class':'ltsearch-translatenameoriginal'})[1]
-#             url = column.find('a', href = re.compile("/en/"))  
-#             link = "http://lyricstranslate.com"+url['href']
-#             lyrics_urls.append(link)
-#     return lyrics_urls
+# get urls of each translation
+def get_all_lyrics_urls(page_urls):
+    lyrics_urls = []
+    for page_url in page_urls:
+        page_soup = connect(page_url)
+        for row in page_soup.findAll('tr')[1:]:
+            column = row.findAll('td',{'class':'ltsearch-translatenameoriginal'})[1]
+            url = column.find('a', href = re.compile("/en/"))  
+            link = "http://lyricstranslate.com"+url['href']
+            lyrics_urls.append(link)
+    return lyrics_urls
 
-# # get artist name from translation
-# def get_artist(lyric_url):
-#     lyrics_soup = connect(lyric_url)
-#     artist = lyrics_soup.find('li',{'class':'song-node-info-artist'}).a.text
-#     return artist
+# get artist name from translation
+def get_artist(lyric_url):
+    lyrics_soup = connect(lyric_url)
+    artist = lyrics_soup.find('li',{'class':'song-node-info-artist'}).a.text
+    return artist
 
-# # get song name in original language (sometimes this is transliterated)
-# def get_song(lyric_url):
-#     lyrics_soup = connect(lyric_url)
-#     song = lyrics_soup.find('div',{'class':'song-node-text'}).find('h2').text
-#     return song
+# get song name in original language (sometimes this is transliterated)
+def get_song(lyric_url):
+    lyrics_soup = connect(lyric_url)
+    song = lyrics_soup.find('div',{'class':'song-node-text'}).find('h2').text
+    return song
 
-# # get name of song, translated
-# def get_english_title(lyric_url):
-#     lyrics_soup = connect(lyric_url)
-#     song = lyrics_soup.find('div',{'class':'translate-node-text '}).find('h2').text
-#     return song
+# get name of song, translated
+def get_english_title(lyric_url):
+    lyrics_soup = connect(lyric_url)
+    song = lyrics_soup.find('div',{'class':'translate-node-text '}).find('h2').text
+    return song
 
-# # get english lyrics
-# def get_english_translation(lyric_url):
-#     lyrics_soup = connect(lyric_url)
-#     paragraphs = lyrics_soup.find('div',{'class':'translate-node-text '}).h2.findNextSiblings('p')
-#     lyrics = []
-#     for paragraph in paragraphs:
-#         paragraph = paragraph.text.splitlines()
-#         lyrics.extend(paragraph)
-#     return lyrics
+# get english lyrics
+def get_english_translation(lyric_url):
+    lyrics_soup = connect(lyric_url)
+    paragraphs = lyrics_soup.find('div',{'class':'translate-node-text '}).h2.findNextSiblings('p')
+    lyrics = []
+    for paragraph in paragraphs:
+        paragraph = paragraph.text.splitlines()
+        lyrics.extend(paragraph)
+    return lyrics
 
-# # create lyrics_urls pickle file to use moving forward:
-# num_pages = 39
-# all_page_urls = get_page_urls(num_pages)
-# print 'got page urls'
-# lyrics_urls = get_all_lyrics_urls(all_page_urls)
-# print 'got lyrics urls'
-# with open('lyrics_urls.pkl', 'w') as picklefile:
-#     pickle.dump(lyrics_urls, picklefile)
+# create lyrics_urls pickle file to use moving forward:
+num_pages = 39
+all_page_urls = get_page_urls(num_pages)
+print 'got page urls'
+lyrics_urls = get_all_lyrics_urls(all_page_urls)
+print 'got lyrics urls'
+with open('lyrics_urls.pkl', 'w') as picklefile:
+    pickle.dump(lyrics_urls, picklefile)
 
-# # create dictionary with all the lyrics and pickle file
-# with open('lyrics_urls.pkl', 'r') as picklefile: 
-#     lyrics_urls = pickle.load(picklefile)
-# all_arabic_songs = {}
-# for i,url in enumerate(lyrics_urls):
-#     print 'started number',i
-#     try:
-#         artist = get_artist(url)
-#     except:
-#         print i, 'problem with artist:', url
-#         break
-#     try:
-#         song = get_song(url)
-#     except:
-#         print i, 'problem with song:',url
-#         break
-#     try:
-#         english_song = get_english_title(url)
-#     except:
-#         print i, 'problem with english title:',url
-#         lyrics_soup = connect(url)
-#         english_song = lyrics_soup.find('div',{'class':'translate-node-text translate-node-text-proofreading'}).find('h2').text
-#     try:
-#         lyrics = get_english_translation(url)
-#     except:
-#         print i, 'problem with lyrics:',url
-#         lyrics_soup = connect(url)
-#         paragraphs = lyrics_soup.find('div',{'class':'translate-node-text translate-node-text-proofreading'}).h2.findNextSiblings('p')
-#         lyrics = []
-#         for paragraph in paragraphs:
-#             paragraph = paragraph.text.splitlines()
-#             lyrics.extend(paragraph)
-#     try:
-#         all_arabic_songs[(artist,song,english_song)]=lyrics
-#     except:
-#         print i, 'problem with dictionary'
-#         break
-# with open('all_arabic_songs.pkl','w') as picklefile:
-#     pickle.dump(all_arabic_songs,picklefile)
+# create dictionary with all the lyrics and pickle file
+with open('lyrics_urls.pkl', 'r') as picklefile: 
+    lyrics_urls = pickle.load(picklefile)
+all_arabic_songs = {}
+for i,url in enumerate(lyrics_urls):
+    print 'started number',i
+    try:
+        artist = get_artist(url)
+    except:
+        print i, 'problem with artist:', url
+        break
+    try:
+        song = get_song(url)
+    except:
+        print i, 'problem with song:',url
+        break
+    try:
+        english_song = get_english_title(url)
+    except:
+        print i, 'problem with english title:',url
+        lyrics_soup = connect(url)
+        english_song = lyrics_soup.find('div',{'class':'translate-node-text translate-node-text-proofreading'}).find('h2').text
+    try:
+        lyrics = get_english_translation(url)
+    except:
+        print i, 'problem with lyrics:',url
+        lyrics_soup = connect(url)
+        paragraphs = lyrics_soup.find('div',{'class':'translate-node-text translate-node-text-proofreading'}).h2.findNextSiblings('p')
+        lyrics = []
+        for paragraph in paragraphs:
+            paragraph = paragraph.text.splitlines()
+            lyrics.extend(paragraph)
+    try:
+        all_arabic_songs[(artist,song,english_song)]=lyrics
+    except:
+        print i, 'problem with dictionary'
+        break
+with open('all_arabic_songs.pkl','w') as picklefile:
+    pickle.dump(all_arabic_songs,picklefile)
 
-# # test new pickle file
-# with open('all_arabic_songs.pkl','r') as picklefile:
-#     all_arabic_songs = pickle.load(picklefile)
+# test new pickle file
+with open('all_arabic_songs.pkl','r') as picklefile:
+    all_arabic_songs = pickle.load(picklefile)
 
 ######################################################################################
 # # getting popular artists and five of their songs from echonest 
@@ -146,58 +160,61 @@ def load_pickle(filename):
 
 config.ECHO_NEST_API_KEY = echonest_API_key
 
-# # get 1000 top artists accoridng to echonest's "hotttnesss" features
-# top_artists = {}
-# for the_artist in artist.top_hottt(results = 1000):
-#     try:
-#         name = the_artist.name
-#         hotness = the_artist.hotttnesss
-#         ID = the_artist.id
-#         top_artists[(name,ID)] = hotness
-#         print name, hotness
-#     except:
-#         print 'waiting one minute'
-#         print 'time now is:', datetime.now()
-#         time.sleep(60)
-#         continue
+# get 1000 top artists accoridng to echonest's "hotttnesss" features
+top_artists = {}
+for the_artist in artist.top_hottt(results = 1000):
+    try:
+        name = the_artist.name
+        hotness = the_artist.hotttnesss
+        ID = the_artist.id
+        top_artists[(name,ID)] = hotness
+        print name, hotness
+    except:
+        print 'waiting one minute'
+        print 'time now is:', datetime.now()
+        time.sleep(60)
+        continue
 
-# with open('top_1000_artists_english.pkl','w') as picklefile:
-#     pickle.dump(top_artists,picklefile)
+with open('top_1000_artists_english.pkl','w') as picklefile:
+    pickle.dump(top_artists,picklefile)
 
-# # get 5 most popular song names from each of the artists
-# with open('top_1000_artists_english.pkl','r') as picklefile:
-#     top_artists = pickle.load(picklefile)
+# get 5 most popular song names from each of the artists
+with open('top_1000_artists_english.pkl','r') as picklefile:
+    top_artists = pickle.load(picklefile)
 
-# artists_and_songs = {} # key = artist, value = song name
-# artists_and_songs_ids = {} # key = artist, value = song id
-# for k in top_artists.keys():
-#     the_artist = k[0]
-#     artist_id = k[1]
-#     try:
-#         songs = artist.Artist(the_artist).get_songs(results=5)
-#         artist_songs = []
-#         song_ids = []
-#         for song in songs[:5]:
-#             artist_songs.append(song)
-#             song_ids.append(song.id)
-#         artists_and_songs[the_artist] = artist_songs
-#         artists_and_songs_ids[the_artist] = song_ids
-#         print the_artist, ':', artist_songs
-#     except:
-#         print 'waiting one minute'
-#         print 'time now is:', datetime.now()
-#         time.sleep(60)
+artists_and_songs = {} # key = artist, value = song name
+artists_and_songs_ids = {} # key = artist, value = song id
+for k in top_artists.keys():
+    the_artist = k[0]
+    artist_id = k[1]
+    try:
+        songs = artist.Artist(the_artist).get_songs(results=5)
+        artist_songs = []
+        song_ids = []
+        for song in songs[:5]:
+            artist_songs.append(song)
+            song_ids.append(song.id)
+        artists_and_songs[the_artist] = artist_songs
+        artists_and_songs_ids[the_artist] = song_ids
+        print the_artist, ':', artist_songs
+    except:
+        print 'waiting one minute'
+        print 'time now is:', datetime.now()
+        time.sleep(60)
 
-# create_pickle(artists_and_songs)
-# create_pickle(artists_and_songs_ids)
+with open('artists_and_songs.pkl','w') as picklefile:
+    pickle.dump(artists_and_songs,picklefile)
+    
+with open('artists_and_songs_id.pkl','w') as picklefile:
+    pickle.dump(artists_and_songs_id,picklefile)
 
 ######################################################################################
 # # getting lyrics of the songs from lyricfind/echonest
 ######################################################################################
     
-# # get lyrics of the ~5000 songs using lyricfind's api
-# artists_and_songs = load_pickle('artists_and_songs.pkl')
-# artists_and_songs_id = load_pickle('artists_and_songs_id.pkl')
+# get lyrics of the ~5000 songs using lyricfind's api
+artists_and_songs = load_pickle('artists_and_songs.pkl')
+artists_and_songs_id = load_pickle('artists_and_songs_id.pkl')
 
 def get_lyrics(song):
     lyricfind_id = song.get_foreign_id('lyricfind-US').replace('lyricfind-US:song:', '')
@@ -217,3 +234,59 @@ def get_lyrics(song):
 ######################################################################################
 # # find arabic songs in echonest or spotify
 ######################################################################################
+
+arabic_songs = load_pickle('all_arabic_songs.pkl') #3515 of them
+
+# steps for getting arabic songs:
+# 1. search for song with artist name
+# 2. if exists, get echonest ID and put in dictionary with same keys
+# 3. and add 1 to count
+# 4. if doesn't exist, output to log
+# 5. at the end, output count of found songs
+
+# import sys
+# sys.exit()
+
+count = 0
+audio_summaries_arabic = {}
+arabic_song_ids = {}
+not_found = []
+for info in arabic_songs.keys(): #for each artist/song combo
+    artist_name = info[0]
+    if artist_name == 'Mohammed Hamaki':
+        artist_name = 'Mohamed Hamaki'
+    if artist_name == 'Majida El Roumi':
+        artist_name = 'Majda El Roumi'
+    if artist_name == 'George Al-Rasi':
+        artist_name = 'Geroge Al Rassi'
+    orig_song_name = info[1]
+    song_name = orig_song_name
+    if '(' in song_name:
+        song_name = remove_paran(song_name)
+    if '-' in song_name:
+        song_name = remove_dashes(song_name)
+    if any(c.isdigit() for c in song_name):
+        song_name = remove_numbers(song_name)
+    translated_song_name = info[2]
+    try:
+        echo_output = song.search(artist=artist_name, title=song_name)
+        if len(echo_output) != 0: #if exists
+            count += 1
+            song_id = echo_output[0].id
+            song_audio_summary = echo_output[0].audio_summary
+            arabic_song_ids[info] = song_id
+            audio_summaries_arabic[info] = song_audio_summary
+            print 'found ' + song_name + ' by ' + artist_name + ' (orig name:) ' + orig_song_name
+        else:
+            print 'could not find ' + song_name + ' by ' + artist_name + ' (orig name:) ' + orig_song_name
+            not_found.append(info)
+    except:
+        print 'waiting one minute'
+        print 'time now is:', datetime.now()
+        time.sleep(60)
+
+create_pickle(audio_summaries_arabic, 'arabic_audio_summaries.pkl')
+create_pickle(arabic_song_ids, 'arabic_song_ids.pkl')
+create_pickle(not_found, 'not_found_songs.pkl')
+
+print 'found ' + str(count) + ' of 3515 songs on echonest'
